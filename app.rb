e06a7504a7c7fecbd2f16feb083a771c08457fef
Kirsten Keels <keelsk@gmail.com>
2020-03-28 18:59:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require_relative 'config/environment'
 require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
+  
   get '/' do 
     erb :index
   end
   
-  get '/create_puppy'
+  get '/create_puppy' do
     erb :create_puppy
   end
   
